87d2ac5f44524b7b7ff789298223c908041a9681
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-23 18:55:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  low_value = nil
+  low_value key = nil
+  name_hash.each do |key, value|
+    if low_value == nil
+      low_value = value
+      low_value key = key
+    else
+      if low_value > value
+        low_value = value
+        low_value_key = key
+      end
+    end
+  end
+  low_value_key
 end
